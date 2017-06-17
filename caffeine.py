15b68fec016ad68bc954f24c893df76a231b4b67
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2017-06-17 21:16:25 +0200

    adds main if

--- caffeine.py
+++ caffeine.py
@@ -21,12 +21,13 @@ def caffeine_contents(drink, serving_size = 500.0):
 	caffeine_per_100 = caffeine_for_drink(drink)
 	return caffeine_per_100 * serving_size / 100.0
 
-if len(sys.argv) > 1:
-	drink = sys.argv[1].lower()
-	if len(sys.argv) > 2:
-		serving = float(sys.argv[2])
-		caffeine = caffeine_contents(drink, serving)
-	else:	
-		caffeine = caffeine_contents(drink)
-	print(caffeine)
+if __name__ == "__main__":
+	if len(sys.argv) > 1:
+		drink = sys.argv[1].lower()
+		if len(sys.argv) > 2:
+			serving = float(sys.argv[2])
+			caffeine = caffeine_contents(drink, serving)
+		else:	
+			caffeine = caffeine_contents(drink)
+		print(caffeine)
 		
