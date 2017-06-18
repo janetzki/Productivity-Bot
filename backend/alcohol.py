35d833cbf2d14208294c40a28113bfc068fdf28d
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2017-06-18 06:37:42 +0200

    fixes alcohol

--- backend/alcohol.py
+++ backend/alcohol.py
@@ -1,6 +1,58 @@
-import datetime
+from datetime import datetime
 
-def calculateBAC(alcDrink, volDrink, water):
-    alc = alcDrink * 0.78924 * volDrink / 100
-    alcInBlood = (alc / water) * 100
-    return alcInBlood
+alcohol_amount = 0.0
+profile = {}
+profile["age"] = "18"
+profile["sex"] = "male"
+profile["weight"] = "65.0"
+alcoholic_drinks = []
+waterPercent = {'male': 0.7, 'female': 0.6 }
+
+def water():
+    return float(profile["weight"]) * float(waterPercent[profile["sex"]])
+
+def set_profile(request):
+    profile["age"] = request["age"]
+    profile["sex"] = request["sex"]
+    profile["weight"] = request["weight"]
+
+def calculate_bac(alc_drink, vol_drink):
+    alc = alc_drink * 0.78924 * vol_drink / 100
+    alc_blood = (alc / water()) * 100
+    return alc_blood
+
+def reduced_bac(bac, seconds):
+    return max(0, bac - 0.15 * (seconds / 3600.0))
+
+def alcohol_for_drink(drink):
+    drink_alc_vol = {}
+    drink_alc_vol["bier"] = 0.05
+    amount = drink_alc_vol.get(drink)
+    if amount is not None:
+        return float(amount)
+    else:
+        contains_matches = [value for key, value in drink_alc_vol.items() if drink in key]
+        if len(contains_matches) > 0:
+            return max(contains_matches)
+        else:
+            return 0.0
+
+def alcohol_contents(drink, serving):
+    global alcohol_amount
+    alc_vol = alcohol_for_drink(drink.lower())
+    current_time = datetime.now()
+    amount = calculate_bac(alc_vol, serving)
+    if len(alcoholic_drinks) > 0:
+        last_element = alcoholic_drinks[-1]
+        diff = (current_time - last_element["timestamp"]).total_seconds()
+        alcohol_amount = reduced_bac(alcohol_amount, diff)
+    alcohol_amount += amount
+    history_object = {}
+    history_object["drink"] = drink
+    history_object["serving"] = serving
+    history_object["alcohol_volume"] = alc_vol
+    history_object["alcohol"] = amount
+    history_object["total_alcohol"] = alcohol_amount
+    history_object["timestamp"] = current_time
+    alcoholic_drinks.append(history_object)
+    return amount
