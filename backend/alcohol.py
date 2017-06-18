c0f1d904b3f49c35afb03e2869aab0cf3dde6a9b
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2017-06-18 11:48:17 +0200

    fixes serving size alcohol

--- backend/alcohol.py
+++ backend/alcohol.py
@@ -33,21 +33,21 @@ def alcohol_for_drink(drink):
     drink_alc_vol = {}
     with open('backend/alcohol_contents.csv', newline='\n') as csvfile:
         for row in csv.reader(csvfile, delimiter=',', quotechar='"'):
-            drink_alc_vol[row[0].lower()] = float(row[1])
-    amount = drink_alc_vol.get(drink)
+            drink_alc_vol[row[0].lower()] = (float(row[1]), float(row[2]))
+    (amount, serving) = drink_alc_vol.get(drink)
     if amount is not None:
-        return float(amount)
+        return (float(amount), serving)
     else:
         contains_matches = [value for key, value in drink_alc_vol.items() if drink in key]
         if len(contains_matches) > 0:
-            return max(contains_matches)
+            return (max(contains_matches), serving)
         else:
-            return 0.0
+            return (0.0, serving)
 
 
 def alcohol_contents(drink, serving, current_time):
     global alcohol_amount
-    alc_vol = alcohol_for_drink(drink.lower())
+    (alc_vol, default_serving) = alcohol_for_drink(drink.lower())
     amount = calculate_bac(alc_vol, serving)
     if amount == 0.0:
         return 0.0
@@ -55,10 +55,14 @@ def alcohol_contents(drink, serving, current_time):
         last_element = alcoholic_drinks[-1]
         diff = (current_time - last_element["timestamp"]).total_seconds()
         alcohol_amount = reduced_bac(alcohol_amount, diff)
+    if serving == 500:
+        used_serving = default_serving
+    else:
+        used_serving = serving
     alcohol_amount += amount
     history_object = {}
     history_object["drink"] = drink.lower()
-    history_object["serving"] = serving
+    history_object["serving"] = used_serving
     history_object["alcohol_volume"] = alc_vol
     history_object["alcohol"] = amount
     history_object["total_alcohol"] = alcohol_amount
